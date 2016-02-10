fe5c4a437de7dbbd564c48665a1c94ab38ff35ec
Victor Lifagin <victor.lifagin@lgsoftlab.ru>
2016-02-10 17:10:46 +0300

    Node corr

--- lab2/Node.h
+++ lab2/Node.h
@@ -2,17 +2,22 @@
 
 class Node
 {
-	Node* pPrev, *pNext;
-	Shape* m_Data;
-	
-	Node(){pPrev=pNext=nullptr;} // used in constructors of List for initialization of Head and Tail
-	Node(Node* p, const Shape* s); // insertion of Node with <s> after Node <p>
-	Node(Node* p, const Node* node); // insertion of already created Node after Node <p>
-	~Node(void); // remove this-Node, delete associated Shape
-	
-	void RemoveNode(); // remove this-Node from the List (delete links)
-	void ConstructNode(Node* p); // create necessary links between Nodes
+    Node* pPrev, *pNext;
+    Shape* m_Data;
 
-	friend class List;
+    // used in constructors of List for initialization of Head and Tail
+    Node()
+        {pPrev = pNext = nullptr;}
+    // insertion of Node with <s> after Node <p>
+    Node(Node* p, const Shape* s);
+    
+    // insertion of already created Node after Node <p>
+    Node(Node* p, const Node* node);
+    ~Node(void); // remove this-Node, delete associated Shape
+    
+    void RemoveNode(); // remove this-Node from the List (delete links)
+    void ConstructNode(Node* p); // create necessary links between Nodes
+
+    friend class List;
 };
 
