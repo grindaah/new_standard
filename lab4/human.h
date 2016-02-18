986c0c445299dcc8234cf9f4ff0836f896b90249
Victor Lifagin <grindaah@gmail.com>
2016-02-18 11:13:57 +0300

    few more lines

--- lab4/human.h
+++ lab4/human.h
@@ -6,14 +6,41 @@ class human
     typedef std::shared_ptr<human> human_ptr;
     typedef std::weak_ptr<human> human_weak_ptr;
 
-    std::string name;
-    bool alive;
-    bool male;
-
     std::pair<human_ptr, human_ptr> parents;
 
     std::vector<human_weak_ptr> children;
 
+    human(){}
+public:
+
+    human (const human& hmn)
+    {
+        std::cout << "cloning not allowed" << std::endl;
+    }
+    human operator = (human&& hmn)
+    {
+        return human (hmn);
+    }
+
+    human (human&& hmn) default;
+
+
+    human(std::string nm, bool al = true)
+    {
+        name = nm;
+        alive = al;
+    }
+    std::string name;
+    bool alive;
+    bool male;
+
+
+    void add_child(human_weak_ptr chld)
+    {
+        children.push_back(chld);
+    }
+    ///get closest children
+    human_ptr next()
+    {}
 };
 
-operator + 
