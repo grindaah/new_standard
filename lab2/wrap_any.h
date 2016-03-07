56c2f484aaf33479cba5ae02a0237fd5aef9eb1f
Victor Lifagin <grindaah@gmail.com>
2016-03-07 10:42:13 +0300

    wrap any v1

--- lab2/wrap_any.h
+++ lab2/wrap_any.h
@@ -7,8 +7,13 @@
 
 class BaseSerial
 {
-public:
+protected:
     std::string m_str;
+public:
+    std::string get_str() const
+    {
+        return m_str;
+    }
 
     friend BaseSerial operator+ (const BaseSerial& lhv, const BaseSerial& rhv)
     {
@@ -17,21 +22,14 @@ public:
         return result;
     }
 
+    BaseSerial& operator+= (const BaseSerial& rhv)
+    {
+        m_str += rhv.get_str();
+        return *this;
+    }
+
     ///Letting the constructors be defaults
-
-//    friend std::ostream& os(std::ostream& os, const BaseSerial& bs);
-//    friend std::ostream& os(std::ostream& os, BaseSerial* bs);
 };
-
-/*std::ostream& os(std::ostream& os, BaseSerial* bs)
-{
-    return os << bs->m_str;
-}
-
-std::ostream& os(std::ostream& os, const BaseSerial& bs)
-{
-    return os << bs.m_str;
-}*/
 
 template<typename T> class Any : public BaseSerial
 {
@@ -47,8 +45,14 @@ public:
 
 class WrapAny
 {
+    ///TODO unique_ptr to make it manage deleting itself
     BaseSerial* m_p;
 public:
+
+    BaseSerial* const get_mp() const
+    {
+        return m_p;
+    }
 
     template<typename T>
     WrapAny(const T& t)
@@ -56,24 +60,29 @@ public:
         m_p = new Any<T>(t);
     }
 
-    template<typename T>
+    /*template<typename T>
     WrapAny(const Any<T>& a)
     {
         m_p = a;
-    }
+    }*/
 
-    //template <typename T1, typename T2>
     friend WrapAny operator+ (const WrapAny& lhv, const WrapAny& rhv)
     {
-        WrapAny result(lhv);
-        *result.m_p = *result.m_p+*rhv.m_p;
+        WrapAny result(std::string(""));
+        *result.m_p = *lhv.m_p + *rhv.m_p;
 
         return result;
     }
 
+    WrapAny& operator +=(const WrapAny& rhv)
+    {
+        *m_p += *rhv.get_mp();
+        return *this;
+    }
+
     friend std::ostream& operator << (std::ostream& os, const WrapAny& w)
     {
-        return os << w.m_p->m_str;
+        return os << w.m_p->get_str();
     }
 };
 
